912c16baad30a0084115961d64c4255225f48dd7
ChrisBlackman <Mr.Christopher.Blackman@gmail.com>
2016-04-01 15:43:11 -0400

    Changes to the compare

--- AlertRules/routes/web.js
+++ AlertRules/routes/web.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const greaterThan = ">";
-const lessThan = "<";
-const equal = "=";
-
-
 
 var jsonObj = {'data': 0}
 
 var genJSON = function() {
-    var newData = Math.floor((Math.random() * 100) + 1);
+    var newData = [Math.floor((Math.random() * 100) + 1)];
     return jsonObj.data = newData;
 }
 
@@ -30,22 +25,57 @@ router.post('/conditions', function(req, res, next) {
     res.send('Success');
 });
 
+//condition 	: (String) that agrees with one fo the constants
+//value			: (object) data[]
+//limit			: (object) limit[]
 var eval = function(condition, value, limit)   {
-    if(condition == greaterThan) {
-        if(value > limit)   {
-            return true;
-        }
-    }
-    else if (condition == lessThan) {
-        if(value < limit)   {
-            return false;
-        }
-    }
-    else if(condition == equal) {
-        if(value == limit)  {
-            return true;
-        }
-    }
+	const greaterThan = ">";
+	const lessThan = "<";
+	const equal = "=";
+	const lessThanEqual = "<=";
+	const greaterThanEqual = ">=";
+	const contains = "exist";
+	const doesNotContain = "notExist";
+	
+	//use typescript to see if the json object is valid
+	if(condition === greaterThan) {
+		return (value > limit);
+	}
+	else if (condition === lessThan) {
+		return (value < limit);
+	}
+	else if(condition === equal) {
+		return (value === limit);
+	}
+	else if(condition === lessThanEqual){
+	return (value <= limit);
+	}
+	else if(condition === greaterThanEqual){
+	return (value >= limit);
+	}
+	else if (condition == contains){
+		for(i = 0;i < value.length;i++){
+			for(j = 0; i < limit.length;j++){
+				if(value[i] === limit[j]){
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+	else if(condition === doesNotContain){
+		for(i = 0;i < value.length;i++){
+			for(j = 0; i < limit.length;j++){
+				if(value[i] === limit[j]){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	else{
+		return false;    
+	}
 }
 
 module.exports = router;

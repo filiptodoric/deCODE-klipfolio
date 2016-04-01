320ae58633645b1c7bec476dc9419f752cba6737
Cole Clifford <coleclifford@Coles-MacBook-Pro.local>
2016-04-01 16:16:47 -0400

    Added sendToTeamTwo function

--- AlertRules/app.js
+++ AlertRules/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
+var request = require('request');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var web = require('./routes/web');
--- AlertRules/routes/web.js
+++ AlertRules/routes/web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 const greaterThan = ">";
 const lessThan = "<";
 const equal = "=";
 
-var jsonObj = {'data': []}
+var jsonObj = {'data': []};
 var genJSON = function() {
     jsonObj.data[jsonObj.data.length] = Math.floor((Math.random() * 100) + 1);
     return jsonObj;
@@ -73,6 +74,25 @@ var eval = function(condition, value, limit)   {
 	else{
 		return false;    
 	}
+}
+
+var sendInfoToTeamTwo = function() {
+    var jsonObj = {
+        'message': {
+            'title': "Fake Title",
+            'body': 'Fake Body'
+        },
+        'config': "Fake Config"
+    };
+    
+    request({
+        url: "",
+        method: "POST",
+        json: true,
+        body: jsonObj
+    }, function(error, response, body) {
+        console.log(response);
+    })
 }
 
 module.exports = router;

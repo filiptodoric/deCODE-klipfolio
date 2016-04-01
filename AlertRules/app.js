ac3074d7cc4a1b0ea7dedb26ad366765e0000450
Joshua Sayavong <jsayavon@uwaterloo.ca>
2016-04-01 12:15:46 -0400

    Changed jade files to ejs files

--- AlertRules/app.js
+++ AlertRules/app.js
@@ -4,15 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var ejs = require('ejs');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+app.engine('html', ejs.renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));

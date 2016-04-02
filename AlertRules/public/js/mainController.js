fe401bb97ec9c2a9cbe847f0b7ea80dfb8754462
Joshua Sayavong <jsayavon@uwaterloo.ca>
2016-04-02 11:22:51 -0400

    logout button

--- AlertRules/public/js/mainController.js
+++ AlertRules/public/js/mainController.js
@@ -5,9 +5,13 @@ define(function () {
 		this._http = $http;
 		this._scope = $scope;
 	 	this.url = "http://localhost:3000/web/conditions";
+	 	this.logoutUrl = "http://localhost:3000/logout";
 		$scope.sendConfig = function(config){
 	    	_this.sendConfig(config);
 	  	};
+	  	$scope.logout = function() {
+	  		_this.logout();
+	  	}
 	};
 
 	controller.prototype.sendConfig = function (ConditionConfig, NotifConfig){
@@ -16,6 +20,14 @@ define(function () {
 			notifConfig: NotifConfig
 		}
 		this._http.post(this.url, config).then( function () {
+			console.log("success");
+		}, function () {
+			console.log("error");
+		});
+	};
+
+	controller.prototype.logout = function (){
+		this._http.post(this.logoutUrl, null).then( function () {
 			console.log("success");
 		}, function () {
 			console.log("error");
